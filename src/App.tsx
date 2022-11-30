40c97caa72fe4f7152d331595d3b890722c8e3f2
João Dias <joaoeduardodias123@gmail.com>
2022-11-30 15:01:47 -0300

    feat: initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+
 export function App() {
-  return <h1>hello word</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>hello word</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'base-title': '#272221',
+  'base-subtitle': '#403937',
+  'base-text': '#574f4d',
+  'base-label': '#8d8686',
+  'base-hover': '#d7d5d5',
+  'base-button': '#e6e5e5',
+  'base-input': '#ededed',
+  'base-card': '#f3f2f2',
+  background: '#fafafa',
+
+  'yellow-dark': '#c47f17',
+  'yellow-light': '#F1E9C9',
+  yellow: '#DBAC2C',
+
+  'purple-dark': '#4B2995',
+  'purple-light': '#EBE5F9',
+  purple: '#8047F8',
+}
