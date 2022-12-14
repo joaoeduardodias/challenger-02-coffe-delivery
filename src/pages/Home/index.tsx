80a298dcc316baa3be8d333db095ca9327e022ea
João Dias <joaoeduardodias123@gmail.com>
2022-12-14 10:03:50 -0300

    feat: section hero

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,51 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import illustrationImage from '../../assets/illustration.svg'
 import { Header } from '../../components/Header'
-import { HomeContainer } from './styles'
+import { HeroContainer, HomeContainer, Icon, ItemsContainer } from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
       <Header />
+
+      <HeroContainer>
+        <div>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
+
+          <ItemsContainer>
+            <div>
+              <Icon backgroundColor="yellowDark">
+                <ShoppingCart size={16} weight="fill" />
+              </Icon>
+
+              <span>Compra simples e segura</span>
+            </div>
+            <div>
+              <Icon backgroundColor="gray">
+                <Package size={16} weight="fill" />
+              </Icon>
+              <span>Embalagem mantém o café intacto</span>
+            </div>
+            <div>
+              <Icon backgroundColor="yellow">
+                <Timer size={16} weight="fill" />
+              </Icon>
+              <span>Entrega rápida e rastreada</span>
+            </div>
+            <div>
+              <Icon backgroundColor="purple">
+                <Coffee size={16} weight="fill" />
+              </Icon>
+              <span>O café chega fresquinho até você</span>
+            </div>
+          </ItemsContainer>
+        </div>
+        <img src={illustrationImage} alt="Copo de café com cereais ao lado" />
+      </HeroContainer>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,70 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.div``
+export const HomeContainer = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+`
+
+export const HeroContainer = styled.section`
+  padding-top: 5.875rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  h1 {
+    color: ${(props) => props.theme['base-title']};
+    font-size: 3rem;
+    font-weight: 800;
+    line-height: 62.4px;
+    font-family: 'Baloo 2', sans-serif;
+    width: 36.75rem;
+  }
+  p {
+    font-size: 1.25rem;
+    line-height: 130%;
+    color: ${(props) => props.theme['base-subtitle']};
+    width: 36.75rem;
+    margin-top: 1rem;
+  }
+  img {
+    width: 29.75rem;
+    height: 22.5rem;
+  }
+`
+
+export const ItemsContainer = styled.div`
+  margin-top: 66px;
+  display: grid;
+  justify-content: initial;
+  grid-template-columns: 1fr 1fr;
+  row-gap: 20px;
+  max-width: 36.75rem;
+
+  div {
+    display: flex;
+    gap: 0.75rem;
+    align-items: center;
+  }
+`
+const BACKGROUND_COLOR = {
+  yellowDark: 'yellow-dark',
+  yellow: 'yellow',
+  gray: 'base-title',
+  purple: 'purple',
+} as const
+
+interface IconProps {
+  backgroundColor: keyof typeof BACKGROUND_COLOR
+}
+
+export const Icon = styled.div<IconProps>`
+  border-radius: 50%;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  color: white;
+  background-color: ${(props) =>
+    props.theme[BACKGROUND_COLOR[props.backgroundColor]]};
+`
